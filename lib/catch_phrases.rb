c807c758000ce88297aeed8b86c8e5f012a79ca5
vien pham <vienpham2019@gmail.com>
2019-12-08 01:31:32 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,10 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  phrase = "It's-a me, Mario"
+  phrase = "It's-a me, Mario!"
   puts phrase 
 end
 
 def toadstool
+  mario
   puts status
 end
